44da751d5416bc97f101f9f46f6697aa45ace283
Sayaliparchande <95363550+Sayaliparchande@users.noreply.github.com>
2021-12-25 20:47:45 +0530

    Week3_Assignments

---
+++ week3/Assignment2/server.js
@@ -0,0 +1,22 @@
+const express=require('express');
+const app=express();
+const port =3000;
+app.get('/',(req,resp)=>{
+    resp.send("WElcome to Express demo");
+})
+app.post('/',(req,resp)=>{
+    resp.send("Called: POST");
+});
+
+app.put('/',(req,resp)=>{
+    resp.send("Called: PUT");
+});
+
+app.delete('/',(req,resp)=>{
+    resp.send("Called: DELETE");
+});
+
+
+app.listen(port,()=>{
+    console.log(`http://localhost:${port} Express server started `);;
+})
